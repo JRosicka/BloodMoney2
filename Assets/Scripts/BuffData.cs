f19a5df1a8d2c86ff0f5362d65089eaaca842a2b
Lane Davis <ldavis@concretesoftware.com>
2022-11-27 11:14:01 -0600

    Very small amount of work toward adding buffs.

---
+++ Assets/Scripts/BuffData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Buff", menuName = "Game Data/Buff")]
+public class BuffData : ScriptableObject {
+    public string ID;
+    public float Duration;
+    public List<AbstractAbilityEffect> Effects;
+    public float EffectPeriod = 0.1f;
+    
+}
